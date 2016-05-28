ca3ce4c31a0e01058cd25ccf29ad21a3e5646544
Rafa <private@email.com>
2016-05-28 15:25:34 +0200

    Several improvements: multiple dns domains, resolve from /etc/hosts, response caching, ipv6 somehow managed, logging improved

--- README.md
+++ README.md
@@ -15,32 +15,38 @@ docker build --rm -t pydns .
 Configuration
 -------------
 
-The server connects to a database and expects request in ports 10053 TCP and UDP. When it receives an A query (and only an A query), in a given domain, it searches for the name in the database and returns the IP address (a single IP address).
+The server connects to a database and expects request in ports 10053 TCP and UDP. When it receives an A query (A, AAAA, A6) in a given domain, it searches for the name in the database and returns the IP address (a single IP address).
 
-The configuration parameters for connection to the database can be given as environment variables:
+The configuration parameters can be given as environment variables:
 
-  - **DB_DRIVER**: the DB-API driver, currently only MySQLdb packed in the container image.
+  - **DB_DRIVER**: the DB-API driver, currently only *MySQLdb* packed in the container image.
   - **DB_HOST**: the database server hostname (and port)
   - **DB_USER**: the database username
   - **DB_PASSWD**: the database password
   - **DB_NAME**: The database name
   - **DB_QUERY**: The query to perform to resolve the name. By default, *"SELECT address FROM dns WHERE query = %s"*
   - **DNS_TTL**: The TTL of A records
+  - **DNS_HOSTS**: Path to the hosts file, defaults to */etc/hosts*
   - **DNS_DOMAIN**: The subdomain for which the server is authoritative
 
 These variables can also be provided in a configuration file in YAML format mounted in **/usr/src/app/config.yml**, like the following:
 
 ```
 ---
-db_driver:  "MySQLdb"
-db_host:    "mysqldb"
-db_user:    "root"
-db_passwd:  "Changeme"
-db_name:    "test"
-db_query:   "SELECT address FROM dns WHERE domain = %s"
-dns_ttl:    3600
-dns_domain: "example.org"
+db_driver:   "MySQLdb"
+db_host:     "mysqldb"
+db_user:     "root"
+db_passwd:   "Changeme"
+db_name:     "test"
+db_query:    "SELECT address FROM dns WHERE domain = %s"
+dns_ttl:      3600
+dns_hosts:   "/etc/hosts"
+dns_domains:
+  - "example.org"
+  - "remote.demo.com"
 ```
+
+As shown above, the configuration file method allows you to specificy several DNS domains, instead of one. **Beware!** the environment parameter is **DNS_DOMAIN** (singular), while the config file parameter is **dns_domains** (plural)!
 
 For example, if you want to be authoritative for a domain "remote.demo.com", create a table "dns" in your mysql server such as:
 
@@ -57,6 +63,10 @@ Then insert your records there, and query them:
 # In MySQL
 INSERT INTO dns VALUES ('test.remote.demo.com', '1.2.3.4');
 
+# You can be recursive if you want, too
+INSERT INTO dns VALUES ('recursive.demo.com', 'www.google.es');
+
 # From the command line
 dig @container_ip -p 10053 test.remote.demo.com
+dig @container_ip -p 10053 recursive.demo.com
 ```
--- dns.py
+++ dns.py
@@ -5,16 +5,20 @@
 An example demonstrating how to create a custom DNS server.
 """
 
-from __future__ import print_function
-
 from twisted.internet import reactor, defer
-from twisted.names import client, dns, error, server
+from twisted.names import client, dns, error, server, hosts, cache
+from twisted.python import log
+from twisted.logger import Logger, textFileLogObserver
 from twisted.enterprise import adbapi
 
 import yaml
 import sys
 import argparse
 import os
+import ipaddress
+
+from textwrap import dedent
+
 
 
 class DynamicResolver(object):
@@ -23,10 +27,53 @@ class DynamicResolver(object):
     query type and name.
     """
 
-    def __init__(self, config, connection, logger):
+    def __init__(self, config, connection, resolver, logger):
+        """
+        Build the resolver.
+        
+        @param config: the configuration object.
+        @param connection: a database connection object.
+        @param resolver: global DNS resolver
+        """
         self._config     = config
         self._connection = connection
+        self._resolver   = resolver
         self._logger     = logger
+
+
+    def _doCreateRecord(self, query, name, representation):
+        """
+        Converts the string representation of an IP address to a
+        RR Record.
+        
+        Raises ValueError if representation is not an address,
+        but a domain name.
+        
+        Raises TypeError if representation is an address, but the
+        IP version of the query and the address mismatch
+        (e.g. queried for an A record but got IPv6 instead)
+        """
+        # ip_address requires unicode strings
+        if sys.version_info <= (3, 0) and not isinstance(representation, unicode):
+            representation = representation.decode('utf8')
+        address     = ipaddress.ip_address(representation)
+        record_type = None
+        # Try to match the record to the representation
+        if address.version == 4 and query.type == dns.A:
+            record_type = dns.Record_A
+        else:
+            if query.type == dns.AAAA:
+                record_type = dns.Record_AAAA
+            elif query.type == dns.A6:
+                record_type = dns.Record_A6
+        # If we couldn't match, raise an Error
+        if record_type is None:
+            raise TypeError("Mismatched query and address")
+        return dns.RRHeader(
+            name=name,
+            payload=record_type(address=str(address)),
+            ttl=self._config.dns_ttl
+        )
 
 
     def _doDynamicResponse(self, query):
@@ -35,28 +82,39 @@ class DynamicResolver(object):
         """
         promise = defer.Deferred()
         name    = query.name.name
-        
-        # Build the record, once the query returns
-        def onResult(result, authority=[], additional=[]):
-            if result:
-                address = result[0][0]
-                self._logger("Query result: %s" % address)
-                answer  = dns.RRHeader(name=name, payload=dns.Record_A(
-                    address=address,
-                    ttl=self._config.dns_ttl
-                ))
-                answers = [answer]
-                promise.callback((answers, authority, additional))
-            else:
-                self._logger("No such domain")
+
+        # Get the info from the database and resolve it
+        def onResult(result, answers=[], additional=[]):
+            if not result:
+                self._logger.info("{data}: No such domain", data=name)
                 promise.errback(error.DomainError())
-        
+                return
+            # First, try to translate the value to an IP address
+            representation = result[0][0]
+            self._logger.debug("SQL query result: {data}" ,
+                data=representation)
+            try:
+                record = self._doCreateRecord(query, name, representation)
+                promise.callback((answers, [record,], additional))
+            # If not an IP address, resolve it recursively
+            except ValueError:
+                query.name.name = representation
+                self._logger.warn("Recursively resolve domain: {data}",
+                    data=representation)
+                entry = self._resolver.query(query, timeout=(3,3,3))
+                entry.chainDeferred(promise)
+            # If mistmatched query and address, return error
+            except TypeError as err:
+                self._logger.failure("Mismatched address and query")
+                promise.errback(error.DomainError())
+            
+        # Error handler, propagates the error back
         def onError(err):
-            self._logger("Query error! %s" % str(err))
-            promise.errback(error.DomainError(err))
+            self._logger.failure("SQL query failed", failure=err)
+            promise.errback(err)
 
         # Run the query
-        entry = self._connection.runQuery(self._config.db_query, name)
+        entry = self._connection.runQuery(self._config.db_query, [name])
         entry.addCallbacks(onResult, onError)
 
         # Return the promise
@@ -68,15 +126,15 @@ class DynamicResolver(object):
         Check if the query should be answered dynamically, otherwise dispatch to
         the fallback resolver.
         """
-        if query.type == dns.A:
+        if query.type in (dns.A, dns.AAAA, dns.A6):
             
             # Check if domain name matches config
             labels = query.name.name.split('.')
-            if ".".join(labels[1:]) == self._config.dns_domain:
+            if ".".join(labels[1:]) in self._config.dns_domains:
 
                 return self._doDynamicResponse(query)
 
-        self._logger("Unexpected query %s" % query.name.name)
+        self._logger.info("Unsupported query: {data}", data=query)
         return defer.fail(error.DomainError())
 
 
@@ -85,44 +143,69 @@ class Config(object):
     Loads the config file
     """
 
-    def __init__(self, path):
+    def __init__(self, path, logger):
         try:
             with open(path, 'r') as cfg:
                 data   = yaml.safe_load(cfg)
                 getter = data.get
-        except:
+        except Exception as err:
+            logger.failure("Error loading config file", failure=err)
             getter = lambda key, default: default
         def top(key, default): 
-            return getter(key, os.getenv(key.upper(), default))
-        # Config file has preference over env var
-        self.db_driver  = top('db_driver',   'MySQLdb')
-        self.db_host    = top('db_host',     '127.0.0.1')
-        self.db_user    = top('db_user',     'root')
-        self.db_passwd  = top('db_passwd',   'Changeme')
-        self.db_name    = top('db_name',     'test')
-        self.db_query   = top('db_query',    'SELECT address FROM dns WHERE domain = %s')
-        self.dns_ttl    = top('dns_ttl',     10)
-        self.dns_domain = top('dns_domain', 'sample.org')
+            # Environment variables have preference over config file
+            return os.getenv(key.upper(), getter(key, default))
+        self.db_driver   = top('db_driver',   'MySQLdb')
+        self.db_host     = top('db_host',     '127.0.0.1')
+        self.db_user     = top('db_user',     'root')
+        self.db_passwd   = top('db_passwd',   'Changeme')
+        self.db_name     = top('db_name',     'test')
+        self.db_query    = top('db_query',    'SELECT address FROM dns WHERE domain = %s')
+        self.dns_ttl     = top('dns_ttl',     300)
+        self.dns_hosts   = top('dns_hosts',   '/etc/hosts')
+        # dns_domains is special because the environment variable
+        # name does not match the config variable name (environment
+        # only allows for one domain)
+        self.dns_domains = os.getenv("DNS_DOMAIN", 
+            getter('dns_domains', 'example.org'))
+        # Turn the domain entry / list into a dict
+        try:
+            # Python 2
+            if isinstance(self.dns_domains, basestring):
+                self.dns_domains = [self.dns_domains,]
+        except NameError:
+            # Python 3
+            if isinstance(self.dns_domains, str):
+                self.dns_domains = [self.dns_domains,]
+        domains = dict()
+        for item in self.dns_domains:
+            domains[item] = True
+        self.dns_domains = domains
+
 
     def __str__(self):
-        return """
-        db_driver: {0.db_driver}
-        db_host: {0.db_host}
-        db_user: {0.db_user}
+        return dedent("""
+        db_driver:   "{0.db_driver}"
+        db_host:     "{0.db_host}"
+        db_user:     "{0.db_user}"
         db_password: ******
-        db_name: {0.db_name}
-        db_query: {0.db_query}
-        dns_ttl: {0.dns_ttl}
-        dns_domain: {0.dns_domain}
-        """.format(self)
-        
+        db_name:     "{0.db_name}"
+        db_query:    "{0.db_query}"
+        dns_ttl:      {0.dns_ttl}
+        dns_hosts:   "{0.dns_hosts}"
+        dns_domains:
+        {1}
+        """).format(self, "\n".join("  - \"%s\"" % domain
+                          for domain in self.dns_domains))
+
+
 
 def main():
     """
     Run the server.
     """
 
-    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser = argparse.ArgumentParser(
+        description='Resolve DNS queries from Database')
     parser.add_argument('-c', '--config',
         dest='config_file',
         type=str, action='store',
@@ -135,26 +218,37 @@ def main():
         default=10053,
         help='Port number for the service'
     )
-    parser.add_argument('--dump', '-d',
-        dest='dump_config',
+    parser.add_argument('--dry-run', '-d',
+        dest='dry_run',
         action='store_true',
-        help='Dry run, just dump the values read from config file'
+        help='Dry run, just check the config file'
     )
-    parser.add_argument('--verbose', '-v',
-        dest='verbose',
-        action='store_true',
-        help='Be verbose'
-    )
+    #parser.add_argument('--verbose', '-v',
+    #    dest='verbose',
+    #    action='store_true',
+    #    help='Be verbose'
+    #)
     params = parser.parse_args()
 
+    # Log to stdout, as this is intended to run in docker
+    log.startLogging(sys.stdout)
+    # Make new logging style compatible to traditional one
+    def observer(event, log=log):
+        log.msg(event['log_format'].format(**event))
+        if 'log_failure' in event:
+            log.err(event['log_failure'])
+    logger = Logger(namespace='default', observer=observer)
+
     # Read config file
-    config = Config(params.config_file)
-    if params.dump_config:
-        print(config)
+    config = Config(params.config_file, logger)
+    logger.debug("Running with the following parameters:\n{data}", data=config)
+
+    # Dry run
+    if params.dry_run:
         sys.exit(0)
     
     # Build a connection lasting the lifetime of the service
-    connection  = adbapi.ConnectionPool(
+    connection = adbapi.ConnectionPool(
         config.db_driver,
         config.db_host,
         config.db_user,
@@ -162,11 +256,20 @@ def main():
         config.db_name
     )
 
+    # Build a global Resolver lasting the lifetime of the service
+    resolver = client.createResolver()
+
     # Factory and protocol services
-    logger   = print if params.verbose else (lambda msg: True)
-    factory  = server.DNSServerFactory(clients=[
-        DynamicResolver(config, connection, logger),
-    ])
+    factory  = server.DNSServerFactory(
+        caches=[
+            cache.CacheResolver(),
+        ],
+        # Use "clients" instead of "authorities", so caching works
+        clients=[
+            hosts.Resolver(file=config.dns_hosts, ttl=config.dns_ttl),
+            DynamicResolver(config, connection, resolver, logger),
+        ]
+    )
     protocol = dns.DNSDatagramProtocol(controller=factory)
 
     # Listen TCP and UDP
